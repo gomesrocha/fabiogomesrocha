fee68bb49eccff7d9b4cbe55da826c533df975ed
Fabio Gomes Rocha <prof.fabiogomesrocha@gmail.com>
2018-01-12 22:21:55 -0300

    atualização da pagina do projeto VLHE
    atualização da pagina do projeto VLHE

---
+++ vlhe.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="pt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Site pessoal Prof. Fabio Gomes Rocha">
+    <title>...:::Fabio Gomes Rocha:::...</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-" crossorigin="anonymous">
+
+    <!--[if lte IE 8]>
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-old-ie-min.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-min.css">
+    <!--<![endif]-->
+
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+
+    <!--[if lte IE 8]>
+    <link rel="stylesheet" href="css/layouts/marketing-old-ie.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+    <link rel="stylesheet" href="css/layouts/marketing.css">
+    <!--<![endif]-->
+</head>
+<body>
+
+<div class="header">
+    <div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
+        <a class="pure-menu-heading" href="">Fabio Gomes Rocha</a>
+
+        <ul class="pure-menu-list">
+            <li class="pure-menu-item"><a href="index.html" class="pure-menu-link">Home</a></li>
+            <li class="pure-menu-item"><a href="materiais.html" class="pure-menu-link">Materiais de apoio</a></li>
+            <li class="pure-menu-item"><a href="publicacao.php" class="pure-menu-link">Publicações</a></li>
+            <li class="pure-menu-item"><a href="http://orcid.org/0000-0002-0512-5406" class="pure-menu-link" target="_blank">ORCID</a></li>
+            <li class="pure-menu-item"><a href="http://lattes.cnpq.br/3161449561865597" target="_blank" class="pure-menu-link">Lattes</a></li>
+            <li class="pure-menu-item"><a href="regras.html"  class="pure-menu-link">Regras de orientação</a></li>
+            <li class="pure-menu-item"><a href="projetos.php"  class="pure-menu-link">Projetos</a></li>
+            <li class="pure-menu-item"><a href="sobre.php"  class="pure-menu-link">Sobre</a></li>
+
+
+        </ul>
+    </div>
+</div>
+
+
+
+<div class="content">
+    <div class="content">
+        <h2 class="content-head is-center">Virtual Library History Education</h2>
+
+        <div class="pure-g">
+            <div class="l-box pure-u-1">
+                <h3 class="content-subhead">
+                    Projeto Virtual Library History Education
+                </h3>
+                    <p>
+                    O Projeto Biblioteca Virtual da Educação visa construir ferramental para pesquisadores da educação empregando o OCR ABBYY para extração de texto e o processamento de linguagem natural para extrair informação, sendo integrada pelos seguintes projetos de IC:
+                    <br />
+                    Projetos do edital 1/2017
+                    <br />
+                    BHExtract – Extração de dados de sites de revistas cientificas nacionais sobre história da educação (Probic)
+                    <br />
+                    Analise de dados de revistas científicas nacionais sobre história da educação (Provic)
+                    <br />
+                    BHImageA: Processamento de imagens históricas
+                </p>
+               
+            </div>
+
+        </div>
+    </div>
+
+    <div class="ribbon l-box-lrg pure-g">
+        <div class="l-box-lrg is-center pure-u-1 pure-u-md-1-2 pure-u-lg-2-5">
+            <img width="300" alt="File Icons" class="pure-img-responsive" src="img/common/file-icons.png">
+        </div>
+        <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-3-5">
+
+            <h2 class="content-head content-head-ribbon">PETIC DECISION MAKING (PDM): UM MODELO AUTOMATIZADO PARA APOIO À TOMADA DE DECISÃO ESTRATÉGICA EM TIC</h2>
+
+            <p>
+                Dissertação do Mestrado em Ciências da Computação da Universidade Federal de Sergipe, desenvolvida por mim sobre o processo de tomada de decisão. O objetivo deste estudo foi a adaptação do Technique for Order Preference by Similarity to Ideal Solution (TOPSIS) na criação de um novo modelo de apoio à tomada de decisão sobre a priorização de investimentos em planejamento estratégico de Tecnologias da Informação e Comunicação como suporte à metodologia PETIC, denominado PETIC Decision Make (PDM).
+                <a href="https://www.researchgate.net/publication/318118771_PETIC_DECISION_MAKING_PDM_UM_MODELO_AUTOMATIZADO_PARA_APOIO_A_TOMADA_DE_DECISAO_ESTRATEGICA_EM_TIC" target="_blank" class="pure-button pure-button-primary">Saiba mais</a>
+            </p>
+        </div>
+    </div>
+
+    <div class="content">
+        <h2 class="content-head is-center">Contato</h2>
+
+        <div class="pure-g">
+            <div class="l-box-lrg pure-u-1 pure-u-md-2-5">
+                <h4>Contato</h4>
+                <p>
+                    gomesrocha at gmail dot com
+                </p>
+
+                <h4>Facebook</h4>
+                <p>
+                    <a href="https://www.facebook.com/prof.fabiogomesrocha" target="_blank">https://www.facebook.com/prof.fabiogomesrocha</a>
+                </p>
+            </div>
+
+            <div class="l-box-lrg pure-u-1 pure-u-md-3-5">
+                <h4>Linkedin</h4>
+                <p>
+                    <a href="https://www.linkedin.com/in/fabiorgomesocha/" target="_blank">https://www.linkedin.com/in/fabiorgomesocha/</a>
+                </p>
+
+                <h4>Twitter</h4>
+                <p>
+                    <a href="https://twitter.com/fabiogomesrocha" target="_blank">@fabiogomesrocha</a>
+                </p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="footer l-box is-center">
+        Prof. Fabio Gomes Rocha    </div>
+
+</div>
+
+
+
+
+</body>
+</html>
