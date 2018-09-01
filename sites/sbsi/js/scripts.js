6eb8b28f7304fb29f9a4615f6e2ea691013a02f4
Fabio Gomes Rocha <gomesrocha@gmail.com>
2018-09-01 17:00:25 -0300

    Sites SBSI
    Inserido os sites SBSI 2019 para analise

---
+++ sites/sbsi/js/scripts.js
@@ -0,0 +1,136 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    // --------------------------------------------------------------------
+    // PreLoader
+    // --------------------------------------------------------------------
+
+    (function () {
+        $('#preloader').delay(200).fadeOut('slow');
+    }());
+
+    // --------------------------------------------------------------------
+    // One Page Navigation
+    // --------------------------------------------------------------------
+
+    (function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() >= 50) {
+                $('nav.navbar').addClass('sticky-nav');
+            }
+            else {
+                $('nav.navbar').removeClass('sticky-nav');
+            }
+        });
+    }());
+
+    // --------------------------------------------------------------------
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    // --------------------------------------------------------------------
+
+    (function () {
+        $('a.page-scroll').on('click', function (e) {
+            e.preventDefault();
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop : $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+        });
+    }());
+
+    // --------------------------------------------------------------------
+    // Closes the Responsive Menu on Menu Item Click
+    // --------------------------------------------------------------------
+
+    (function () {
+        $('.navbar-collapse ul li a').on('click', function () {
+            if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+                $('.navbar-toggle:visible').trigger('click');
+            }
+        });
+    }());
+
+    // --------------------------------------------------------------------
+    // Google Map
+    // --------------------------------------------------------------------
+
+    (function () {
+        if ($('#googleMap').length > 0) {
+
+            //set your google maps parameters
+            var $latitude  = -10.968280, //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
+                $longitude = -37.058649,
+                $map_zoom  = 17;
+            /* ZOOM SETTING */
+
+            //google map custom marker icon
+            var $marker_url = 'img/icones/crab.png';
+
+            //we define here the style of the map
+            var style = [{
+                "stylers" : [{
+                    "hue" : "#E43724"
+                }, {
+                    "saturation" : 25
+                }, {
+                    "gamma" : 0.90
+                }, {
+                    "lightness" : 0
+                }]
+            }];
+
+            //set google map options
+            var map_options = {
+                center            : new google.maps.LatLng($latitude, $longitude),
+                zoom              : $map_zoom,
+                panControl        : true,
+                zoomControl       : true,
+                mapTypeControl    : false,
+                streetViewControl : false,
+                mapTypeId         : google.maps.MapTypeId.ROADMAP,
+                scrollwheel       : false,
+                styles            : style,
+            }
+            //initialize the map
+            var map = new google.maps.Map(document.getElementById('googleMap'), map_options);
+            //add a custom marker to the map
+            var marker = new google.maps.Marker({
+                position : new google.maps.LatLng($latitude, $longitude),
+                map      : map,
+                visible  : true,
+                icon     :$marker_url
+            });
+        }
+    }());
+
+    // Set the date we're counting down to
+    var countDownDate = new Date("May 20, 2019 9:00:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+        // Get todays date and time
+        var now = new Date().getTime();
+        
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
+        
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        // Output the result in an element with id="demo"
+        document.getElementById("timer").innerHTML = + days + "<span> dias  </span>: " + hours + "<span> horas</span>: "
+        + minutes + "<span> minutos  </span>: " + seconds + "<span> segundos  </span>";
+        
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+
+}); // JQuery end
